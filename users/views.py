7b54c144a0938ec8f654fcf09fe996e984407480
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-03 22:27:11 +0300

    Created userRegisterView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import UserRegisterSerializer
+from .models import User
+
+from rest_framework import viewsets
+
+class UserRegisterView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
