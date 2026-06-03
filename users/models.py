5ccf5592dc7ce03d890e99afe5694d626dddb857
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-03 21:50:23 +0300

    Postgresql databse configuration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('client', 'Client'),
+    )
+    role = models.CharField(max_length=20,choices=ROLE_CHOICES,default="client")
+    email  = models.EmailField(unique=True)
+    phone = models.CharField(max_length=30, unique=True)
+    is_verified = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    
+    def __str__(self):
+        return f"{self.email}"
+
