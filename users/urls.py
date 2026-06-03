8d2dbbfd42b57d8f2e447398d7163a22e58becd9
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-03 22:32:38 +0300

    created urls.py file and configured it using rest_framework routers

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+router = routers.DefaultRouter()
+
+from .views import UserRegisterView
+
+router.register(r"register",UserRegisterView)
+
+urlpatterns = [
+    path("", include(router.url))
+]
+
+
