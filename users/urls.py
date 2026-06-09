79a8fd2070538314ba225db4becac9d9669e2770
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-09 18:56:20 +0300

    Updated the RegisterView

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from rest_framework import routers
 router = routers.DefaultRouter()
 
-from .views import UserRegisterView
+from .views import RegisterView
 
-router.register(r"register",UserRegisterView)
+router.register(r"register",RegisterView)
 
 urlpatterns = [
     path("", include(router.urls))
--- users/views.py
+++ users/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, LoginSerializer
 from .models import User
-
+from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import send_mail
 
-class UserRegisterView(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserRegisterSerializer
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response({
+                "message":"User registered successfully"
+                
+            }, status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors,status=400)    
+            
+        
+    
+    
+  
