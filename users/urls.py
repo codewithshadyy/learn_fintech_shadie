945b11813b9fce3d0195ef34545720844b86984e
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-09 23:41:15 +0300

    urls configuration

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from .views import RegisterView, LoginView , LogoutView, ForgotPasswordView
+from .views import RegisterView, LoginView , LogoutView, ForgotPasswordView, PasswordResetView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -8,7 +8,8 @@ urlpatterns = [
     path("login", LoginView.as_view()),
     path("logout", LogoutView.as_view()),
     path("token/refresh", TokenRefreshView.as_view()),
-    path("password-forgot", ForgotPasswordView.as_view())
+    path("password-forgot", ForgotPasswordView.as_view()),
+    path("password_reset/<uidb64>/<token>", PasswordResetView().as_view())
     
 ]
 
