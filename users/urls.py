523ac226b59cf9fe57ecfc07ba5836389555a309
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-09 21:11:35 +0300

    fixed the url path errors

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("/register", RegisterView.as_view()),
-    path("/login", LoginView.as_view())
+    path("register", RegisterView.as_view()),
+    path("login", LoginView.as_view())
     
 ]
 
